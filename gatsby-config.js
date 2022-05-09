68409d43ef7cc8c41f7920d53b44f796ead092e6
Jihee <62944687+ivy-love@users.noreply.github.com>
2022-05-09 17:49:39 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Hello World !!!`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Jihee`,
+      summary: `publisher`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
